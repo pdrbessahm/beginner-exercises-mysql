9be6b28dd9a65d6708843634682d06329bae2136
pdrbessahm <bessapedro500@gmail.com>
2025-09-07 13:40:11 -0300

    Exercise 3

---
+++ 03_populating_table.sql
@@ -0,0 +1,7 @@
+USE company_db;
+
+-- Insert at least 5 rows into the employees table with different names, ages, departments, and salaries.
+INSERT INTO employees (emp_name, age, department, salary) 
+VALUES ('Alice', 25, 'IT', 48000.00), ('Bob', 32, 'HR', 42000.00),
+('Carol', 29, 'Sales', 54000.00), ('David', 40, 'IT', 72000.00),
+('Eva', 35, 'Marketing', 60000.00);
